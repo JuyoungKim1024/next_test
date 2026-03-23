66cbc379fe962bd942fe92c9650832a5cdafdb15
Kim Juyoung <op45672@gmail.com>
2026-03-23 12:42:35 +0900

    step10

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,13 +1,32 @@
+'use client'
+import { useState } from 'react'
+
 export default function SignUp() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('회원가입 정보:', { email, password })
+  }
+
   return (
-    <form>
-      <input type="email" name="email" placeholder="이메일을 입력하세요" />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name="email"
+        placeholder="이메일을 입력하세요"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <input
         type="password"
         name="password"
         placeholder="비밀번호를 입력하세요"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <button>회원가입</button>
+      <button style={{ cursor: 'pointer' }}>회원가입</button>
     </form>
   )
 }

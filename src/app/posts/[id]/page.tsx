7ca1cdb05391b8d88555adb090e5001eacd1cd73
Kim Juyoung <op45672@gmail.com>
2026-03-23 11:11:51 +0900

    step8

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,23 +1,42 @@
 'use client'
 
+import { supabase } from '@/src/lib/supabase'
 import { useParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
+interface Post {
+  id: number
+  created_at: string
+  title: string
+  content: string
+}
+
 export default function PostDetail() {
   const { id } = useParams()
-  const [post, setPost] = useState({})
+  const [posts, setPosts] = useState<Post | null>(null)
+
+  const fetchPost = async () => {
+    const { data: post, error } = await supabase
+      .from('posts')
+      .select('*')
+      .eq('id', id)
+      .single()
+    setPosts(post)
+  }
 
   useEffect(() => {
-    fetch(`https://dummyjson.com/posts/${id}`)
-      .then((res) => res.json())
-      .then((res) => setPost(res))
+    fetchPost()
   }, [])
+
+  if (!posts) {
+    return <div>로딩중...</div>
+  }
 
   return (
     <>
-      <div>{post.id}번 게시글 상세</div>
-      <div>{post.title}</div>
-      <div>{post.body}</div>
+      <div>{posts.id}번 게시글 상세</div>
+      <div>{posts.title}</div>
+      <div>{posts.content}</div>
     </>
   )
 }
